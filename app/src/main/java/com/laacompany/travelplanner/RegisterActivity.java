e4267e4e869a43654a30ae95612b753c03e6df49
jennifergoldwin <jennifer.goldwin@binus.ac.id>
2020-05-31 17:14:43 +0700

    commit by jen

--- app/src/main/java/com/laacompany/travelplanner/RegisterActivity.java
+++ app/src/main/java/com/laacompany/travelplanner/RegisterActivity.java
@@ -6,6 +6,8 @@ import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -13,22 +15,31 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.laacompany.travelplanner.Storage.RegisterData;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
+import java.util.Objects;
 
 import static com.laacompany.travelplanner.Storage.DataStorage.registerData;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    public static EditText mEDTUsername, mEDTPassword, mEDTConfirmPassword, mEDTPhone, mEDTbirthDate;
-    public static Button mBTNBack, mBTNRegister;
+    public static TextInputEditText mEDTUsername, mEDTPassword, mEDTConfirmPassword, mEDTPhone, mEDTDof;
+    public static TextInputLayout mLAYUsername, mLAYPassword, mLAYConfirmPassword, mLAYPhone, mLAYDof;
+    public static TextView mTXTBack;
+    public static Button mBTNRegister;
     public static RadioGroup mRGGroup;
     public static RadioButton mRBButton;
     public static CheckBox mCBCheckBox;
     public static DatePickerDialog dpdialog;
+    public static SimpleDateFormat dateFormatter;
 
     public static Intent newIntent(Context packageContext){
         return new Intent(packageContext,RegisterActivity.class);
@@ -41,109 +52,163 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         init();
 
-        mEDTbirthDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final Calendar c = Calendar.getInstance();
-                int mYear = c.get(Calendar.YEAR);
-                int mMonth = c.get(Calendar.MONTH);
-                int mDay = c.get(Calendar.DAY_OF_MONTH);
 
-                dpdialog = new DatePickerDialog(RegisterActivity.this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        mEDTbirthDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
-                    }
-                }, mYear, mMonth, mDay);
-                dpdialog.show();
-            }
+        mTXTBack.setOnClickListener(v -> {
+            startActivity(new Intent(this,LoginActivity.class));
+            this.finish();
         });
 
-        mBTNRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String uname,pass,cfpass,pnum,birthdate,gender;
-                //Name 3 - 25 lenght containt at least 1 alphabetic and digit
-                String regUname = "(?=.*[0-9])(?=.*[a-zA-Z]).{3,25}";
-                //Contain 1 lowercase , 1 upper case , 1 digit
-                String regPass = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,}";
-                uname = mEDTUsername.getText().toString();
-                pass = mEDTPassword.getText().toString();
-                cfpass = mEDTConfirmPassword.getText().toString();
-                pnum = mEDTPhone.getText().toString();
-                birthdate = mEDTbirthDate.getText().toString();
-                int getRadioValue = mRGGroup.getCheckedRadioButtonId();
-                mRBButton = findViewById(getRadioValue);
+        mEDTDof.setOnClickListener(v -> {
+            final Calendar c = Calendar.getInstance();
+            int mYear = c.get(Calendar.YEAR);
+            int mMonth = c.get(Calendar.MONTH);
+            int mDay = c.get(Calendar.DAY_OF_MONTH);
 
-                if(uname.trim().equals("") || pass.trim().equals("") || cfpass.trim().equals("") || pnum.trim().equals("")
-                        || birthdate.trim().equals("")){
-                    Toast.makeText(RegisterActivity.this,"Please fill all column",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(uname.length() < 3){
-                    Toast.makeText(RegisterActivity.this,"Username too short",Toast.LENGTH_SHORT).show();
-                    return;
-                }else if(uname.length() > 25){
-                    Toast.makeText(RegisterActivity.this,"Username too long",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(uname.matches(regUname) == false){
-                    Toast.makeText(RegisterActivity.this,"Please contain 1 digit",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(pass.length() < 6){
-                    Toast.makeText(RegisterActivity.this,"Password too short",Toast.LENGTH_SHORT).show();
-                    return;
-                }
+            dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
 
-                //Confirmation regex password & compare password
-                if(pass.matches(regPass) == false){
-                    Toast.makeText(RegisterActivity.this,"Please contain 1 uppercase and 1 digit",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(cfpass.compareTo(pass) != 0){
-                    Toast.makeText(RegisterActivity.this,"Password not same",Toast.LENGTH_SHORT).show();
-                    return;
-                }
+            dpdialog = new DatePickerDialog(RegisterActivity.this, (view, year, monthOfYear, dayOfMonth) -> {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year,monthOfYear,dayOfMonth);
 
-                //phonenumber check
-                if(pnum.length() < 10){
-                    Toast.makeText(RegisterActivity.this,"Phone number too short",Toast.LENGTH_SHORT).show();
-                    return;
-                }else if(pnum.length() > 12){
-                    Toast.makeText(RegisterActivity.this,"Phone number too long",Toast.LENGTH_SHORT).show();
-                    return;
-                }
+                mEDTDof.setText(dateFormatter.format(newDate.getTime()));
+                mLAYDof.setError(null);
+            }, mYear, mMonth, mDay);
 
-                //radiobutton
-                if(mRGGroup.getCheckedRadioButtonId() == -1){
-                    Toast.makeText(RegisterActivity.this,"Please choose your gender",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                //checkbox
-                if(!mCBCheckBox.isChecked()){
-                    Toast.makeText(RegisterActivity.this,"Please fill the checkbox",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                gender = mRBButton.getText().toString();
+            dpdialog.getDatePicker().setMaxDate(c.getTimeInMillis());
+            dpdialog.show();
 
+        });
 
-                //Toast Go here and insert data
-                registerData.add(new RegisterData(uname,pass,pnum,gender,birthdate));
-            }
+        mBTNRegister.setOnClickListener(v -> {
+
+            checkRegister();
         });
 
     }
 
+    private void checkRegister() {
+        boolean registerValid = true;
+
+        String uname,pass="",pnum,birthdate,gender;
+
+
+        int getRadioValue = mRGGroup.getCheckedRadioButtonId();
+        mRBButton = findViewById(getRadioValue);
+
+
+
+        //USERNAME CHECK
+        if (!mEDTUsername.getText().toString().matches("^(?=.*\\d)(?=.*[a-zA-Z]).{3,25}$")){
+            mLAYUsername.setError("Username must between 3 and 25 characters,1 digit and alphabetic");
+            registerValid = false;
+        }
+        else mLAYUsername.setError(null);
+
+        //PASSWORD CHECK
+
+        if (!mEDTPassword.getText().toString().matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{6,}$") ) {
+            mLAYPassword.setError("Password must contain at least 1 lowercase letter, 1 uppercase letter,1 digit and more than 6 character");
+            registerValid = false;
+        }
+        else {
+            mLAYPassword.setError(null);
+            pass = mEDTPassword.getText().toString();
+
+            //CONFIRM PASSWORD CHECK
+
+            if (!mEDTConfirmPassword.getText().toString().matches(pass)){
+                mLAYConfirmPassword.setError("Must same with password");
+                registerValid = false;
+            }
+            else {
+                mLAYConfirmPassword.setError(null);
+            }
+        }
+
+        //PHONE NUMBER CHECK
+        boolean flag = true;
+        int len = mEDTPhone.getText().toString().length();
+        if (len>6){
+            for (int i =0 ; i<len ; i++){
+                if (!(mEDTPhone.getText().toString().charAt(i)>='0' &&
+                        mEDTPhone.getText().toString().charAt(i)<='9'))
+                    flag = false;
+            }
+            if (flag) mLAYPhone.setError(null);
+
+            else{
+                mLAYPhone.setError("Phone number must contains digit only");
+                registerValid = false;
+            }
+
+        }
+        else {
+            mLAYPhone.setError("Phone number must between 10 and 12 character");
+            registerValid = false;
+        }
+
+        //DOF CHECK
+        if (isEmpty(mEDTDof)){
+            mLAYDof.setError("This input must be filled");
+            registerValid = false;
+        }
+
+        //RADIO BUTTON CHECK
+
+        if(mRGGroup.getCheckedRadioButtonId() == -1){
+            Toast.makeText(RegisterActivity.this,"Please choose your gender",Toast.LENGTH_SHORT).show();
+            registerValid = false;
+        }
+
+        //CHECKBOX CHECK
+
+        if(!mCBCheckBox.isChecked()){
+            Toast.makeText(RegisterActivity.this,"Please fill the checkbox",Toast.LENGTH_SHORT).show();
+            registerValid = false;
+        }
+
+        if (registerValid){
+
+            /**USERNAME EXISTS CHECK HERE
+             your code here
+             **/
+
+            //ALL VALID GOES HERE
+            uname = mEDTUsername.getText().toString();
+            pnum = mEDTPhone.getText().toString();
+            birthdate = Objects.requireNonNull(mEDTDof.getText()).toString();
+            gender = mRBButton.getText().toString();
+
+            //Toast Goes here and insert data
+            registerData.add(new RegisterData(uname,pass,pnum,gender,birthdate));
+            Toast.makeText(getApplicationContext(),"Register Successful",Toast.LENGTH_SHORT).show();
+            RegisterActivity.this.finish();
+        }
+    }
+
     void init(){
+        mLAYUsername = findViewById(R.id.id_activity_register_iptlayout_username);
+        mLAYPassword = findViewById(R.id.id_activity_register_iptlayout_password);
+        mLAYConfirmPassword = findViewById(R.id.id_activity_register_iptlayout_confirm_password);
+        mLAYDof = findViewById(R.id.id_activity_register_iptlayout_dof);
+        mLAYPhone = findViewById(R.id.id_activity_register_iptlayout_phone);
+
+
         mEDTUsername = findViewById(R.id.id_activity_register_edt_username);
         mEDTPassword = findViewById(R.id.id_activity_register_edt_password);
         mEDTConfirmPassword = findViewById(R.id.id_activity_register_edt_confirmation_password);
         mEDTPhone = findViewById(R.id.id_activity_register_edt_phone);
-        mEDTbirthDate = findViewById(R.id.id_activity_register_edt_birthDate);
-        mBTNBack = findViewById(R.id.id_activity_register_btn_back);
+        mEDTDof = findViewById(R.id.id_activity_register_edt_birthDate);
+        mTXTBack = findViewById(R.id.id_activity_register_btn_back);
         mBTNRegister = findViewById(R.id.id_activity_register_btn_register);
         mRGGroup = findViewById(R.id.id_activity_register_rd_rdGroup);
         mCBCheckBox = findViewById(R.id.id_activity_register_cb_cbox);
+
+        mEDTDof.setInputType(InputType.TYPE_NULL);
+    }
+
+    boolean isEmpty(TextInputEditText text){
+        String checktext = text.getText().toString();
+        return TextUtils.isEmpty(checktext);
     }
 }
